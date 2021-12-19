9c13e35e877586a19c618530a2d92752a5693221
Recognizeyourself <recognizeyourself909@gmail.com>
2021-12-19 13:25:00 +0530

    Require The bodyParser module and parse app using "urlencoded"
    In order for us to actually get the text that the user typed into the 
    input, we have to do a little. This is just the necessary code for us to 
    be able to start parsing through the body of the post request.

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const express = require('express');
 
 // Calling the express function
 const app = express();
+
+// Requiring the bodyParser Module
+const bodyParser = require('body-parser');
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
